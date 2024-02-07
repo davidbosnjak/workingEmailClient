40d567c1e5893b1f6476100108dc7b28b34159dd
davidbosnjak <107808522+davidbosnjak@users.noreply.github.com>
2024-02-06 19:02:34 -0500

    Update openAI.py

--- src/openAI.py
+++ src/openAI.py
@@ -2,8 +2,8 @@ import os
 import openai
 import sys
 
-
-openai.api_key = ("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")
+my_key = ""
+openai.api_key = (my_key)
 
 response = openai.Completion.create(model="text-davinci-003", prompt=sys.argv[1], temperature=0.7, max_tokens=1200)
 text = response['choices'][0]['text']
